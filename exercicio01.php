ecd50cbec4b066a598f3e2277959bd221562743e
matheusbohrer <matheusalvesbohrer@gmail.com>
2025-08-22 11:38:19 -0300

    .

--- exercicio01.php
+++ exercicio01.php
@@ -6,6 +6,24 @@
     <title>Exercício 01 - Par ou Ímpar</title>
 </head>
 <body>
-    
+    <form method="" action="">
+        <label for="numero">Digite um número</label>
+        <input type="number" id="numero" name="numero" require>
+        <button type="submit" name="verificar_par_impar">Verificar</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if(isset($_POST['verificar_par_impar'])) {
+            $numero = $_POST['numero'];
+            
+            if ($numero % 2 == 0) {
+                echo "O número $numero é <strong>par</strong>.";
+            } else {
+                echo "O número $numero é <strong>impar</strong>.";
+            }
+        }
+    }
+    ?>
 </body>
 </html>
